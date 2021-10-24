acf294c074fd5ebdfb2cd5773833b94e3c7a948e
Larissa SilvaNovaes <Larissa.SilvaNovaes@jsafrasarasin.com>
2021-10-23 22:49:40 -0300

    add card-checkbox component

--- src/app/components/card-checkbox/card-checkbox.component.ts
+++ src/app/components/card-checkbox/card-checkbox.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-checkbox',
   templateUrl: './card-checkbox.component.html',
-  styleUrls: ['./card-checkbox.component.scss']
+  styleUrls: ['./card-checkbox.component.scss'],
 })
 export class CardCheckboxComponent implements OnInit {
+  @Input() imgUrl: string = '';
+  @Input() title: string = '';
+  @Input() text: string = '';
+  @Input() linkPath: string = '';
+  @Input() active: boolean = false;
+  @Input() tabIndex: number = 0;
+  @Input() id: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/stepper-cotacao/stepper-cotacao.component.ts
+++ src/app/pages/stepper-cotacao/stepper-cotacao.component.ts
@@ -1,6 +1,5 @@
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component, OnInit } from '@angular/core';
-import { Answers } from './../../models/answers.model';
 import { Question } from './../../models/question.model';
 import { QuestionsService } from './../../services/questions/questions.service';
 
@@ -19,17 +18,18 @@ export class StepperCotacaoComponent implements OnInit {
   h1Text = 'quer saber quanto custa seu seguro?';
   h1TextSecondary = 'comece respondendo as perguntas abaixo e descubra :)';
 
+  imgUrl =
+    'https://itau-mkt-place-seguros.s3-sa-east-1.amazonaws.com/images/residencial/ilustracoes/incendio.svg';
+  title = 'incêndio, explosão, implosão, fumaça e queda de aeronave';
+
   questions: Question[] = [];
-  answers: Answers[] = [];
 
   constructor(private questionsService: QuestionsService) {}
 
   ngOnInit(): void {
     this.questionsService.getQuestions().subscribe((e: Question[]) => {
       this.questions = e;
-      e.map((e) => console.log('===>>>>', e.respostas));
     });
-    console.log(this.answers);
   }
 
   reset = () => {
