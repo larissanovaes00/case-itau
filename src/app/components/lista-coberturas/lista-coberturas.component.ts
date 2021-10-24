262acfd0496560a9f6a971c3c63974f25f541d4b
Larissa SilvaNovaes <larissanovaes00@hotmail.com>
2021-10-24 13:25:30 -0300

    CardModel

--- src/app/components/lista-coberturas/lista-coberturas.component.ts
+++ src/app/components/lista-coberturas/lista-coberturas.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { CardModel } from '../../models/card.model'
 @Component({
   selector: 'app-lista-coberturas',
   templateUrl: './lista-coberturas.component.html',
   styleUrls: ['./lista-coberturas.component.scss'],
 })
 export class ListaCoberturasComponent implements OnInit {
+  @Input() cardInput: CardModel[] = [];
+
   imgUrl =
     'https://itau-mkt-place-seguros.s3-sa-east-1.amazonaws.com/images/residencial/ilustracoes/incendio.svg';
   title = 'incêndio, explosão, implosão, fumaça e queda de aeronave';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.cardInput);
+
+  }
+
 }
