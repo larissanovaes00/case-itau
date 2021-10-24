52576e26c08702f073c4867e93267d608190b1f9
Larissa SilvaNovaes <larissanovaes00@hotmail.com>
2021-10-24 12:30:56 -0300

    Unit tests

--- src/app/services/offers/offers.service.spec.ts
+++ src/app/services/offers/offers.service.spec.ts
@@ -1,11 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { OfertasService } from './offers.service';
 
 describe('Service: Ofertas', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [OfertasService]
     });
   });
